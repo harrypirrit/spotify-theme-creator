0d722ca93eeae013ccd64a99fc625ef0546e4e95
Harry Pirrit <harrypirrit@icloud.com>
2024-08-28 13:46:31 +1200

    created Header
    created Layout for content

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-white shadow-sm">
+      <div className="max-w-full">
+        <h1 className="text-lg font-semibold text-gray-900 py-3 px-4">
+          Spicetify Theme Creator
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
